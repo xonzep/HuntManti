aef7096db336c85cedd1f76c4e5ecac487173b26
Jeff McElhare <jmcelhare@gmail.com>
2023-06-02 11:50:06 -0400

    Cleaned up file.

--- HuntManti/Program.cs
+++ HuntManti/Program.cs
@@ -4,31 +4,6 @@
  * The first thing to do is setup the state of the game. For that we need a few variables to hold our starting points.
  * Next we need to get info from the first player and store that. We create a function to get the number which we can reuse for the
  * second player's input as well.
- *
- * The first user begins by secretly establishing how far the Manticore is from the city, in the range 0 to 100.
- * The program then allows a second player to repeatedly attempt to destroy the airship by picking the
- * range to target until either the city of Consolas or the Manticore is destroyed. In each attempt, the player
- * is told if they overshot (too far), fell short (not far enough), or hit the Manticore. The damage dealt to the
- * Manticore depends on the turn number. For most turns, 1 point of damage is dealt. But if the turn number
- * is a multiple of 3, a fire blast deals 3 points of damage; a multiple of 5, an electric blast deals 3 points of
- * damage, and if it is a multiple of both 3 and 5, a mighty fire-electric blast deals 10 points of damage. The
- * Manticore is destroyed after taking 10 points of damage.
- * However, if the Manticore survives a turn, it will deal a guaranteed 1 point of damage to the city of
- * Consolas. The city can only take 15 points of damage before being annihilated.
- * Before a round begins, the user should see the current status: the current round number, the city’s health,
- * and the Manticore’s health.
- *
- * STATUS: Round: 1 City: 15/15 Manticore: 10/10
- *The cannon is expected to deal 1 damage this round.
- *
- *--------------------------------------
- *
- * 
- *Enter desired cannon range: 50
- *That round OVERSHOT the target.
- *
- * We need to take in the user's guess, compare it to the position and if higher or lower we report back.
- * We also need to up the damage the manticore does each round.
 */
 
 
